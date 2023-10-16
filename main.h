ad276c6fde81ad25b4b5ebc74d4e62c6afc60cc5
Elsmoil <alsmoilalrasta999@gmail.com>
2023-10-16 16:34:41 -0700

    more

--- main.h
+++ main.h
@@ -1,5 +1,5 @@
 #ifndef _PRINTF_H
-#define _PRUNTF_H
+#define _PRINTF_H
 
 #include <stdarg.h>
 #include <stdio.h>
@@ -23,54 +23,69 @@
  *
  * @unsign: flag if unsigned value
  *
- * @space_flag: on if hashtag_flag specified
- * @hashtag_flag: on if _flag specified
- * @zero_flag: on if _flag specified
- * @minus_flag: on if _flag specified
- * @plus_flag: on if plus_flag specified
+ * @space_flag: on if hashtag_flag specified.
+ * @hashtag_flag: on if _flag specified.
+ * @zero_flag: on if _flag specified.
+ * @minus_flag: on if _flag specified.
+ * @plus_flag: on if plus_flag specified.
  *
- * @width: field width specified
- * @precision: field precision specified
+ * @width: field width specified.
+ * @precision: field precision specified.
  *
- * l_modifier: on if l_modifier is specified
- * h_modifier: on if h_modifier is specified
+ * l_modifier: on if l_modifier is specified.
+ * h_modifier: on if h_modifier is specified.
  */
+
 typedef struct parameters
 {
-	unsigned int unsign		: 1;
+	unsigned int unsign	: 1;
 
-	unsigned int plus_flag		: 1;
-	unsigned int space_flag		: 1;
+	unsigned int plus_flag	: 1;
+	unsigned int space_flag	: 1;
 	unsigned int hashtag_flag	: 1;
-	unsigned int zero_flag		: 1;
-	unsigned int minus_flag		: 1;
+	unsigned int zero_flag	: 1;
+	unsigned int minus_flag	: 1;
 
 	unsigned int width;
 	unsigned int precision;
 
-	unsigned int l_modifier		: 1;
-	unsigned int h_modifier		: 1;
+	unsigned int l_modifier	: 1;
+	unsigned int h_modifier	: 1;
 } params_t;
 
 /**
  * struct specifier - struct token
  *
  * @specifier: format token
- * @f: The function
+ * @f: The function associated
  */
+
 typedef struct specifier
 {
 	char *specifier;
-	int (*f) (va_list, params_t *);
+	int (*f)(va_list, params_t *);
 } specifier_t;
 
+/* _put.c module */
 int _puts(char *str);
 int _putchar(int c);
 
+/* print_functions.c module */
 int print_char(va_list ap, params_t *params);
 int print_int(va_list ap, params_t *params);
 int print_string(va_list ap, params_t *params);
 int print_percent(va_list ap, params_t *params);
 int print_S(va_list ap, params_t *params);
+
+/*number.c module */
+char *convert(long int num, int base, int flags, params_t *params);
+int print_unsigned(va_list ap, params_t *params);
+int print_address(va_list ap, params_t *params);
+
+/* string_fields.c module */
+char *get_precision(char *p, params_t *params, va_list ap);
+
+/* _printf.c module */
+int _printf(const char *format, ...);
 
 #endif
